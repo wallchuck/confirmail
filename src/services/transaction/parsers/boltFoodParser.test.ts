65f29b20b6d649aa34dc210686124d452de65d3d
wallchuck <wallchuck@wallchuck.net>
2022-10-18 19:52:33 +0200

    Regenerate existing parsers for consistency

--- src/services/transaction/parsers/boltFoodParser.test.ts
+++ src/services/transaction/parsers/boltFoodParser.test.ts
@@ -6,54 +6,53 @@ import { boltFoodParser } from "./boltFoodParser";
 describe("boltFoodParser", () => {
   it("reads transaction details from message text", async () => {
     const messageText = `
-      ---------- Forwarded message --------- 
-      From: Bolt Food <poland-food@bolt.eu> 
-      Date: Fri, 17 Jun 2022 at 13:39 
-      Subject: Delivery from Bolt Food 
-      To: <example@example.com> 
+---------- Forwarded message ---------
+From: Bolt Food <poland-food@bolt.eu>
+Date: Wed, 2 Mar 2022 at 18:48
+Subject: Delivery from Bolt Food
+To: <example@example.com>
 
 
-      ­17.06.2022 
+­02.03.2022
 
 
-      *Bon Appetit, John!* 
+*Bon Appetit, John!*
 
-      This is your receipt. 
-      From United India ­UL. KAROLA BORSUKA 21 
-      To ­Example Street 4/20, Warszawa, Polska 
+This is your receipt.
+From Bistro Gruzińskie Granat ­Branickiego 10 lok.111
+To ­Testowa 1, Warszawa, Polska
 
-      1 × Prawns Papadi (8pcs) Packaging 
+1 × Charczo
 
-      32.20 ZŁ 
+14.45 ZŁ
 
-      Delivery fee 
+1 × Chinkali with Meat (4 pieces)
 
-      4.89 ZŁ 
+22.10 ZŁ
 
-      Small order fee 
+1 × Shoti
 
-      2.80 ZŁ 
+4.25 ZŁ
 
-      Campaign: XSKXNNN5X1DY6L6 
+Delivery fee
 
-      -4.89 ZŁ 
+7.44 ZŁ
 
-      *Total charged:* 
+*Total charged:*
 
-      *35.00 ZŁ* 
+*48.24 ZŁ*
 
-      •••• 0000 
+•••• 0000
 
-      Download cost document Delivery 
-      <https://delivery-invoicing.bolt.eu/invoice/pdf/?uuid=00000000-0000-0000-0000-000000000000&l=gb&c=pl>
-      If you require an invoice for Food, please request it from the Seller. 
-      Bolt Operations OÜ
-    `;
+Download cost document Delivery
+<https://delivery-invoicing.bolt.eu/invoice/pdf/?uuid=fd3abda4-54ac-469e-bb6d-b547a3acc002&l=gb&c=pl>
+If you require an invoice for Food, please request it from the Seller.
+Bolt Operations OÜ`;
 
     const transactionDetails: TransactionDetails = {
-      date: parseISO("2022-06-17"),
-      amount: 35.0,
-      memo: "United India",
+      date: parseISO("2022-03-02"),
+      amount: 48.24,
+      memo: "Bistro Gruzińskie Granat",
     };
 
     expect(boltFoodParser(messageText)).toStrictEqual(transactionDetails);
@@ -61,98 +60,96 @@ describe("boltFoodParser", () => {
 
   it("throws if date string cannot be parsed", async () => {
     const messageText = `
-      ---------- Forwarded message --------- 
-      From: Bolt Food <poland-food@bolt.eu> 
-      Date: Fri, 17 Jun 2022 at 13:39 
-      Subject: Delivery from Bolt Food 
-      To: <example@example.com> 
+---------- Forwarded message ---------
+From: Bolt Food <poland-food@bolt.eu>
+Date: Wed, 2 Mar 2022 at 18:48
+Subject: Delivery from Bolt Food
+To: <example@example.com>
 
 
-      17.06.2022 
+­02/03/2022
 
 
-      *Bon Appetit, John!* 
+*Bon Appetit, John!*
 
-      This is your receipt. 
-      From United India ­UL. KAROLA BORSUKA 21 
-      To ­Example Street 4/20, Warszawa, Polska 
+This is your receipt.
+From Bistro Gruzińskie Granat ­Branickiego 10 lok.111
+To ­Testowa 1, Warszawa, Polska
 
-      1 × Prawns Papadi (8pcs) Packaging 
+1 × Charczo
 
-      32.20 ZŁ 
+14.45 ZŁ
 
-      Delivery fee 
+1 × Chinkali with Meat (4 pieces)
 
-      4.89 ZŁ 
+22.10 ZŁ
 
-      Small order fee 
+1 × Shoti
 
-      2.80 ZŁ 
+4.25 ZŁ
 
-      Campaign: XSKXNNN5X1DY6L6 
+Delivery fee
 
-      -4.89 ZŁ 
+7.44 ZŁ
 
-      *Total charged:* 
+*Total charged:*
 
-      *35.00 ZŁ* 
+*48.24 ZŁ*
 
-      •••• 0000 
+•••• 0000
 
-      Download cost document Delivery 
-      <https://delivery-invoicing.bolt.eu/invoice/pdf/?uuid=00000000-0000-0000-0000-000000000000&l=gb&c=pl>
-      If you require an invoice for Food, please request it from the Seller. 
-      Bolt Operations OÜ
-    `;
+Download cost document Delivery
+<https://delivery-invoicing.bolt.eu/invoice/pdf/?uuid=fd3abda4-54ac-469e-bb6d-b547a3acc002&l=gb&c=pl>
+If you require an invoice for Food, please request it from the Seller.
+Bolt Operations OÜ`;
 
     expect(() => boltFoodParser(messageText)).toThrow();
   });
 
   it("throws if amount string cannot be parsed", async () => {
     const messageText = `
-      ---------- Forwarded message --------- 
-      From: Bolt Food <poland-food@bolt.eu> 
-      Date: Fri, 17 Jun 2022 at 13:39 
-      Subject: Delivery from Bolt Food 
-      To: <example@example.com> 
+---------- Forwarded message ---------
+From: Bolt Food <poland-food@bolt.eu>
+Date: Wed, 2 Mar 2022 at 18:48
+Subject: Delivery from Bolt Food
+To: <example@example.com>
 
 
-      ­17.06.2022 
+­02.03.2022
 
 
-      *Bon Appetit, John!* 
+*Bon Appetit, John!*
 
-      This is your receipt. 
-      From United India ­UL. KAROLA BORSUKA 21 
-      To ­Example Street 4/20, Warszawa, Polska 
+This is your receipt.
+From Bistro Gruzińskie Granat ­Branickiego 10 lok.111
+To ­Testowa 1, Warszawa, Polska
 
-      1 × Prawns Papadi (8pcs) Packaging 
+1 × Charczo
 
-      32.20 ZŁ 
+14.45 ZŁ
 
-      Delivery fee 
+1 × Chinkali with Meat (4 pieces)
 
-      4.89 ZŁ 
+22.10 ZŁ
 
-      Small order fee 
+1 × Shoti
 
-      2.80 ZŁ 
+4.25 ZŁ
 
-      Campaign: XSKXNNN5X1DY6L6 
+Delivery fee
 
-      -4.89 ZŁ 
+7.44 ZŁ
 
-      *Total charged:* 
+*Total charged:*
 
-      *ZŁ* 
+*48,24 ZŁ*
 
-      •••• 0000 
+•••• 0000
 
-      Download cost document Delivery 
-      <https://delivery-invoicing.bolt.eu/invoice/pdf/?uuid=00000000-0000-0000-0000-000000000000&l=gb&c=pl>
-      If you require an invoice for Food, please request it from the Seller. 
-      Bolt Operations OÜ
-    `;
+Download cost document Delivery
+<https://delivery-invoicing.bolt.eu/invoice/pdf/?uuid=fd3abda4-54ac-469e-bb6d-b547a3acc002&l=gb&c=pl>
+If you require an invoice for Food, please request it from the Seller.
+Bolt Operations OÜ`;
 
     expect(() => boltFoodParser(messageText)).toThrow();
   });
--- src/services/transaction/parsers/boltFoodParser.ts
+++ src/services/transaction/parsers/boltFoodParser.ts
@@ -4,6 +4,6 @@ export const boltFoodParser = parserFactory({
   yearRegExp: /\u00AD\d+\.\d+\.(\d+)/,
   monthRegExp: /\u00AD\d+\.(\d+)/,
   dayRegExp: /\u00AD(\d+)/,
-  amountRegExp: /Total charged:[\S\s]*?(\d+[\d\s]*\.\d\d)/,
+  amountRegExp: /Total charged:[\S\s]*?(\d+\.\d\d)/,
   memoRegExp: /From (.*) \u00AD/,
 });
